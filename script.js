e85f5b3bcec5c39a1ed0209cd957ba3e1e508893
Leandro <leandro_finten@hotmail.com>
2023-09-21 21:05:38 -0300

    json

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-const div1 = document.querySelector(".desktop4__4")
-console.log(div1);
-
-fetch("")
+fetch("https://my-json-server.typicode.com/leandrofinten123/Leandro-Finten-entrega01/Experiencia1")
+.then((respuesta) => {
+    console.log(respuesta);
+    if(!respuesta.ok) throw new Error(`${respuesta.status}`);
+    return respuesta.json();
+})
+.then((experiencias) => {
+    console.log(experiencias);
+    const div1 = document.querySelector(".desktop4");
+    div1.innerHTML = experiencias
+    .map((exp) => `<p>${exp.titulo}</p>
+    <h3>
+      ${exp.Subtitulo}
+    </h3>`
+      )
+    .join("");
+})
+.catch((error) => {
+    console.log("catch", error);
+})
